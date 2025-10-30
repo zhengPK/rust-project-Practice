e1c333b8983de90eebe3f16514dde83205e92630
ZhengPK <18300622503@163.com>
2025-10-30 15:32:39 +0800

    feat:3.1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+
+async fn greet(req:HttpRequest) -> impl Responder{
+    let name = req.match_info().get("name").unwrap_or("world");
+    format!("Hello {}!", &name)
+}
+async fn health_check(_req:HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+#[tokio::main]
+async fn main() -> std::io::Result<()>{
+    HttpServer::new(||{
+        App::new()
+        .route("/", web::get().to(greet))
+        .route("/{name}", web::get().to(greet))
+        .route("/health_check", web::get().to(health_check))
+        
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
