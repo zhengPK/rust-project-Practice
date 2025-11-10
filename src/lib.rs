d2c910d8c533cc3de4c1e265be26490b679d5cb6
ZhengPK <18300622503@163.com>
2025-11-10 10:15:00 +0800

    feat: 完成3.5继承测试

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,16 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 use actix_web::dev::Server;
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
 // async fn greet(req:HttpRequest) -> impl Responder{
 //     let name = req.match_info().get("name").unwrap_or("world");
 //     format!("Hello {}!", &name)
 // }
-async fn health_check(_req:HttpRequest) -> impl Responder {
+async fn health_check(_req: HttpRequest) -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub fn run() -> Result<Server,std::io::Error> {
-    let server = HttpServer::new(||{
-        App::new()
-        .route("/health_check", web::get().to(health_check))
-        
-    })
-    .bind("127.0.0.1:8000")?
-    .run();
+pub fn run() -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
     Ok(server)
 }
